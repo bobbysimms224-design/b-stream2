daef4c1d800311cc1b8bc67419a9a7d6f5039afc
bobbysimms224-design <bobbysimms224@gmail.com>
2025-11-03 09:58:27 -0500

    Update LinksDropdown.tsx

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -305,7 +305,7 @@ export function LinksDropdown(props: { children: React.ReactNode }) {
             />
             <CircleDropdownLink href="/support" icon={Icons.SUPPORT} />
             <CircleDropdownLink
-              href="https://rentry.co/h5mypdfs"
+              href="https://rentry.co/abe2977z"
               icon={Icons.TIP_JAR}
             />
           </div>
